f76120a55e530d926deb5d7ea2dfaa1fd6a58952
mohamed-chafik <mchafik2006@gmail.com>
2025-07-15 14:42:29 +0100

    init

---
+++ src/components/header.jsx
@@ -0,0 +1,11 @@
+function Header(){
+  return <>
+    <section>
+    <div>
+    <h1 class='font-rock' >mohamed Chafik</h1>
+    </div>
+    </section>
+    
+    </>
+}
+export default Header;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["*"],
+    theme: {
+        extend: {
+            fontFamily: {
+                "lato": ["Rock Salt"]
+            }
+        },
+    },
+    plugins: [],
+}
