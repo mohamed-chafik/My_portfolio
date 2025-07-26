e80e850e3e457db3884f93bc6e1056a904d42c18
mohamed-chafik <mchafik2006@gmail.com>
2025-07-26 11:58:36 +0000

    adding projects

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,13 +1,19 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faTerminal } from '@fortawesome/free-solid-svg-icons';
 import { faCircle } from '@fortawesome/free-solid-svg-icons';
+import projects from '../projects.json';
+import React,{useState} from 'react'
 function Projects(){
+    const [hoveredProject, setHoveredProject] = useState(null);
   return <>
-       <section class='w-full h-[100vh]'>
-          <h2 class="font-acme text-4xl items-center flex pl-[17px] text-white mb-[20px]"><FontAwesomeIcon  icon={faTerminal} size="xs" class='text-[#00f050] w-[36px]'/>Technical Skills</h2>
-        <div id ='project' class='text-white border-b-[#3d3d3d] border-b border-solid w-[65%] hover:text-[#00f050]'>
+       <section class='w-full h-fit'>
+          <h2 class="font-acme text-4xl items-center flex pl-[17px] text-white mb-[20px]"><FontAwesomeIcon  icon={faTerminal} size="xs" class='text-[#00f050] w-[36px]'/>Projects</h2>
+         {projects.map((project) => (
+          <div class='w-full h-fit flex justify-evenly'>      
+      <div class='w-[65%]'>
+        <div id ='project' class='text-white border-b-[#3d3d3d] border-b border-solid w-full hover:text-[#00f050]'>
           <div>
-            <h1 class='text-6xl font-oswald font-bold '>Projet Number 1</h1>
+            <h1 class='text-6xl font-oswald font-bold '>{project.Name}</h1>
           </div>
           <div>
             <p class='fonr-intel text-[#afafaf]'><ul>
@@ -18,10 +24,16 @@ function Projects(){
             </span>
           </div>
         </div>
+        </div> 
+
         <div class='w-[30%] h-fit'>
-        <img src="#" class='w-full h-full' alt=""/>
+        <img src={project.Image} class='w-full h-full' alt=""/>
       </div>
-        </section> 
+      </div>
+
+
+         ))}  
+    </section> 
 
   </>
 }
