60ad191b037cee819acf810929bbf83a1ac16c1b
mohamed-chafik <mchafik2006@gmail.com>
2025-07-28 11:11:37 +0000

    last tuches

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 import Header from './components/header.jsx'
 import Content from './components/content.jsx'
 import Projects from './components/projects.jsx'
+
 function App() {
 
 
@@ -13,6 +14,7 @@ function App() {
       <Header></Header>
       <Content/>
       <Projects/>
+
 
   
     </>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,25 @@
 import React, { useEffect, useRef } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faGithub } from "@fortawesome/free-brands-svg-icons";
+import { faLinkedin} from "@fortawesome/free-brands-svg-icons";
+import Button from './resume.jsx'
 function Header() {
     return (
-        <section className=' w-full h-[100vh] flex justify-center items-center  bg-center z-0' id='head'>
-            <div className="w-fit h-fit flex flex-col items-center justify-center bg-draw z-3" >
+        <section className=' w-full h-[100vh] flex justify-center items-center  bg-center z-0 inline-grid' id='head'>
+          <div class='self-right self-baseline text-white pointer mr-[11px] pt-[11px]'>
+            <ul class='flex justify-end'>
+              <li class='w-fit mr-[20px] cursor-pointer'><a href=""><FontAwesomeIcon icon={faGithub} size="2xl" className='w-[25px]'/></a></li>
+              <li class='w-fit mr-[20px] cursor-pointer'><a href=""><FontAwesomeIcon icon={faLinkedin} size="2xl" className='w-[25px]'/></a></li>
+            </ul>
+          </div>
+            <div className="w-fit h-fit flex flex-col items-center justify-center bg-draw z-3 self-baseline" >
                 <h1 className='font-rock text-8xl text-white '>Mohamed Chafik</h1>
                 <p className='w-fit mt-[30px] font-fira text-white'>Front-End Developer | Minimal Code, Maximal Impact</p>
+                <ul class='flex'>
+
+                  <li ><Button ></Button></li>
+ 
+                </ul>
             </div>
         </section>
     );
